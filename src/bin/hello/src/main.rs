049ac113e65890b5d8b840e9526b8169fa04d5e8
zphrs <zejones@ucsc.edu>
2024-05-31 00:01:51 +0000

    got hello world working, improved develop.md documentation with findings

--- src/bin/hello/src/main.rs
+++ src/bin/hello/src/main.rs
@@ -1,3 +1,9 @@
+use twizzler_abi::pager::{CompletionToKernel, KernelCompletionData, RequestFromKernel};
+
+async fn handle_request(_request: RequestFromKernel) -> Option<CompletionToKernel> {
+    Some(CompletionToKernel::new(KernelCompletionData::EchoResp))
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world");
 }
