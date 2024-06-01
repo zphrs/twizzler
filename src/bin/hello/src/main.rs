486f188b45ef308f5884a0e24746b34f18cab882
zphrs <zejones@ucsc.edu>
2024-06-01 01:53:50 +0000

    slightly modified hello's main.rs to be simpler

--- src/bin/hello/src/main.rs
+++ src/bin/hello/src/main.rs
@@ -1,8 +1,4 @@
-use twizzler_abi::pager::{CompletionToKernel, KernelCompletionData, RequestFromKernel};
-
-async fn handle_request(_request: RequestFromKernel) -> Option<CompletionToKernel> {
-    Some(CompletionToKernel::new(KernelCompletionData::EchoResp))
-}
+extern crate twizzler_abi;
 
 fn main() {
     println!("Hello, world");
