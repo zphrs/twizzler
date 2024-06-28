0c8ba5e3c770045f454cba742c0082e3723e64ac
zphrs <46908127+zphyrj@users.noreply.github.com>
2024-06-27 23:50:40 -0700

    got crypto working within the kernel!!

--- src/kernel/src/crypto.rs
+++ src/kernel/src/crypto.rs
@@ -1,8 +1,21 @@
-// use p256::ecdsa::{signature::Signer, Signature, SigningKey};
-// use sha2::{Digest, Sha256, Sha512};
+use p256::ecdsa::{
+    signature,
+    signature::{Signer, Verifier},
+    Signature, SigningKey, VerifyingKey,
+};
+use sha2::{digest::generic_array::GenericArray, Digest, Sha256, Sha512};
 
-// pub fn sha256(input: impl AsRef<[u8]>) {
-//     let mut hasher = Sha256::new();
-//     hasher.update(input);
-//     let res = hasher.finalize();
-// }
+pub fn sha256(input: impl AsRef<[u8]>) -> impl Iterator<Item = u8> {
+    let mut hasher = Sha256::new();
+    hasher.update(input);
+    let res = hasher.finalize();
+    res.into_iter()
+}
+
+pub fn sign(key: SigningKey, message: &[u8]) -> Signature {
+    key.sign(message)
+}
+
+pub fn verify(key: VerifyingKey, message: &[u8], signature: Signature) -> signature::Result<()> {
+    key.verify(message, &signature)
+}
